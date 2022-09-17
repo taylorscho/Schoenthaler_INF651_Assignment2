035b6f3263cdd13580886504ecb1bdbd917507ef
taylorscho <tlsells3@mail.fhsu.edu>
2022-09-17 14:10:28 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+//Question 1
+let myNumber;
+
+//Question 2
+myNumber = 5;
+
+//Question 3
+let myOtherNumber = 5;
+
+//Question 4
+let myTotal;
+myTotal = myNumber * myOtherNumber;
+console.log(myTotal);
+
+//Question 5
+let myName = '';
+let myCombo = '';
+myCombo = string(myNumber + myOtherNumber);
+console.log(myCombo);
+
+//Question 6
+let myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+console.log(myOtherCombo);
+
+//Question 7
+let myRemainder;
+myRemainder = myNumber % 5;
+console.log(myRemainder);
+
+//Question 8
+let i = 2;
+
+//Question 9
+let int = 4;
+
+//Question 10
+let myBoolean = False;
+console.log(myBoolean);
